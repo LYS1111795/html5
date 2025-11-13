a76bfbabb1c9ce133b86c341261f150fe92cba13
Your Name <you@example.com>
2025-11-13 09:26:50 +0900

    first

--- portfolio/lush/js/main.js
+++ portfolio/lush/js/main.js
@@ -21,22 +21,17 @@ window.onscroll = function () {
 };
 
 const shareImgTarget = document.querySelector(".share_img img");
-
+const container = shareImgTarget.parentElement;
 function handleMoveMotion() {
-  const container = shareImgTarget.parentElement;
   const rect = container.getBoundingClientRect();
 
   if (rect.top < window.innerHeight && rect.bottom > 0) {
-    // ⭐ movementRate: 스크롤 속도의 '아주 미세한 3% (0.03)'만 반영합니다.
-    const movementRate = 0.05;
-
-    // 뷰포트 중앙 (window.innerHeight / 2)을 기준으로 이미지가 움직이도록 계산합니다.
-    // rect.top 값이 변함에 따라 moveY 값이 미세하게 변합니다.
-    const viewCenter = window.innerHeight / 5;
-    const moveY = (rect.top - viewCenter) * movementRate;
+    const movementRate = 0.07;
+    const viewCenter = window.innerHeight / 2;
+    const offset = rect.top - viewCenter;
+    const moveY = offset * movementRate;
     shareImgTarget.style.transform = `translate3d(0px, ${moveY}px, 0px) scale(1.1)`;
   }
 }
-
 window.addEventListener("scroll", handleMoveMotion);
 handleMoveMotion();
